5d4bc28c36096100cc843c9eebcb7e9915521662
Deric Cheung <cheung.deric@gmail.com>
2022-08-22 11:05:43 -0600

    Update ANTLR to v4.10.1

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,8 @@ int main(int argc, char **argv) {
   }
 
   // Open the file then parse and lex it.
-  antlr4::ANTLRFileStream afs(argv[1]);
+  antlr4::ANTLRFileStream afs;
+  afs.loadFromFile(argv[1]);
   gazprea::GazpreaLexer lexer(&afs);
   antlr4::CommonTokenStream tokens(&lexer);
   gazprea::GazpreaParser parser(&tokens);
